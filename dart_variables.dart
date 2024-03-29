cf16886116b1521b70c56a57c00a10c23be39597
Andrew256 <141121118+Andrew256CRO@users.noreply.github.com>
2024-03-29 11:13:54 +0200

    Add files via upload

---
+++ dart_variables.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  var firstVarEver='Hello there'; // declar o varoabila de tip String ce contine *ceva*
+  print(firstVarEver); // print = printeaza
+
+  print('Acum schimb continutul variabilei.');
+  firstVarEver='Andrei'; // modific continutul variabilei
+  print("Noua valoare a variabilei este:" + firstVarEver);
+
+  // firstVarEver=324; //Error: A value of type 'int' can't be assigned to a variable of type 'String'.
+}
